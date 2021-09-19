746619d71225b72d255caa6ede354b6b4bca1a49
rajdesai30 <rajdesai3092@gmail.com>
2021-09-19 12:18:17 +0530

    first reactwebsite

---
+++ rd/src/Sdata.jsx
@@ -0,0 +1,34 @@
+import web from "./images/web.jpg";
+import app from "./images/app.jpg";
+import android from "./images/android.jpg";
+import digital from "./images/digital.jpg";
+import marketing from "./images/marketing.jpg";
+import software from "./images/software.jpg";
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title: "Web development",
+    },
+    {
+        imgsrc: app,
+        title: "App development",
+    },
+    {
+        imgsrc: android,
+        title: "Application development",
+    },
+    {
+        imgsrc: software,
+        title: "Software development",
+    },
+    {
+        imgsrc: digital,
+        title: "Digital Marketing",
+    },
+    {
+        imgsrc: marketing,
+        title: "Marketing",
+    }
+];
+export default Sdata;
